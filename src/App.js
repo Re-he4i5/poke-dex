1f746ac87ac4da3f0295e35025c704a3f4c89cb8
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-08-04 14:25:08 +0900

    load_data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { getAllPokemon, getPokemon } from "./utils/pokemon";
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -17,13 +18,14 @@ function App() {
     fetchPokemonData();
   }, []);
 
-  const loadPokemon = (data) => {
-    let _pokemonData = Promise.all(
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
       data.map((pokemon) => {
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
     );
+    setPokemonData(_pokemonData);
   };
 
   return (
