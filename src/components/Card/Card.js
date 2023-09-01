689cb68d1f9d63af69fcdd7dce8addd948a03d4e
Re-he4i5 <kuronekoryohei@gmail.com>
2023-09-02 01:19:20 +0900

    名前、画像などの取得

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Card = ({ pokemon }) => {
-  return <div>Card</div>;
+  console.log(pokemon);
+  return (
+    <div class="card">
+      <div class="card__image-container">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <h3>{pokemon.name}</h3>
+        <h4>
+          {pokemon.types.map((type) => {
+            return <p>{type.type.name}</p>;
+          })}
+        </h4>
+        <div className="cardInfo">
+          <div className="cardData">
+            <p className="title">重さ: {pokemon.weight} </p>
+          </div>
+          <div className="cardData">
+            <p className="title">高さ: {pokemon.height} </p>
+          </div>
+          <div className="cardData">
+            <p className="title">
+              アビリティ: {pokemon.abilities[0].ability.name}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
