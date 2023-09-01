f38e5f438e19db41984ee217546f829e3c3c6a62
Re-he4i5 <kuronekoryohei@gmail.com>
2023-09-02 02:23:17 +0900

    ページめくり機能の追加

--- src/App.js
+++ src/App.js
@@ -8,16 +8,38 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       loadPokemon(res.results);
       // console.log(res);
+      setNextURL(res.next);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const handlePrevPage = async () => {
+    if (!prevURL) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setPrevURL(data.previous);
+    setNextURL(data.next);
+    setLoading(false);
+  };
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setPrevURL(data.previous);
+    setNextURL(data.next);
+    setLoading(false);
+  };
 
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
@@ -42,6 +64,10 @@ function App() {
             {pokemonData.map((pokemon, i) => {
               return <Card key={i} pokemon={pokemon} />;
             })}
+          </div>
+          <div className="btn">
+            <button onClick={handlePrevPage}>前へ</button>
+            <button onClick={handleNextPage}>次へ</button>
           </div>
         </>
       )}
