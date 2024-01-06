cc427b50d0f0e04b7455ec0fe4d392d74b22509c
recodeyo1ko <kuronekoryohei@gmail.com>
2024-01-06 22:48:47 +0900

    pokemon apiの取得

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { getPokemon, getAllPokemon } from "./utils/pokemon";
+
+interface PokemonResponse {
+  results: Pokemon[];
+}
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokemonListResponse {
+  results: Array<{ name: string; url: string }>;
+}
 
 function App() {
+  const [pokemonData, setPokemonData] = React.useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const response = (await getAllPokemon(
+        `https://pokeapi.co/api/v2/pokemon`
+      )) as PokemonListResponse;
+      const pokemonList = response.results;
+
+      console.log(pokemonList);
+    };
+    fetchPokemonData();
+  }, []);
+
   return <div className="App"></div>;
 }
 
