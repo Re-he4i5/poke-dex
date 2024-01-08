138231a5d1ae6c00ea7638736a984ae8ac96c381
recodeyo1ko <kuronekoryohei@gmail.com>
2024-01-08 22:33:46 +0900

    indexのレイアウト調整

--- front/src/components/Card.tsx
+++ front/src/components/Card.tsx
@@ -12,7 +12,7 @@ interface PokemonProps {
 
 const Card: React.FC<PokemonProps> = ({ pokemon }) => {
   return (
-    <div>
+    <div className="card">
       <div className="cardImg">
         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       </div>
